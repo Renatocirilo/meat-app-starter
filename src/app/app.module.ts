863e25b6d601f04250fdad797d683e45e267a806
Renatocirilo <renato_cirilo@hotmail.com>
2019-08-05 23:28:13 -0300

    adding sub-component restaurant and categorys

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,11 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, AboutComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, AboutComponent, RestaurantsComponent, RestaurantComponent],
   imports: [BrowserModule, HttpModule, RouterModule.forRoot(ROUTES)],
   providers: [],
   bootstrap: [AppComponent]
