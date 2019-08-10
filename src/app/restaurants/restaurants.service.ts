d7e11b83f83fe49890a8833b212b28085ae10dd5
Renatocirilo <renato_cirilo@hotmail.com>
2019-08-10 09:04:02 -0300

    Error Handling with the Catch Operator

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -2,9 +2,11 @@ import { Injectable } from "@angular/core";
 import { Http } from "@angular/http";
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
 
 import { Restaurant } from "./restaurant/restaurant.model";
 import { MEAT_API } from "../app.api";
+import { ErrorHandler } from "../app.error-handler";
 
 @Injectable()
 export class RestaurantsService {
@@ -14,5 +16,6 @@ export class RestaurantsService {
     return this.https
       .get(`${MEAT_API}/restaurants`)
       .map(response => response.json());
+    // .catch(ErrorHandler.hendleError);
   }
 }
