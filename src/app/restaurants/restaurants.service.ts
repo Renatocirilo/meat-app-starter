c4e6f315579897ed57db138ece1e1b0bee8af988
Renatocirilo <renato_cirilo@hotmail.com>
2019-08-10 15:44:57 -0300

    Serviço HTTP para o Detalhe do Restaurante

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -10,12 +10,19 @@ import { ErrorHandler } from "../app.error-handler";
 
 @Injectable()
 export class RestaurantsService {
-  constructor(private https: Http) {}
+  constructor(private http: Http) {}
 
   restaurants(): Observable<Restaurant[]> {
-    return this.https
+    return this.http
       .get(`${MEAT_API}/restaurants`)
       .map(response => response.json());
-    // .catch(ErrorHandler.hendleError);
+    // .catch(ErrorHandler.handleError);
+  }
+
+  restaurantById(id: string): Observable<Restaurant> {
+    return this.http
+      .get(`${MEAT_API}/restaurants/${id}`)
+      .map(response => response.json());
+    // .catch(ErrorHandler.handleError);
   }
 }
