1f919dcff10bbb49d9648ee726a21a8d420fb012
Renatocirilo <renato_cirilo@hotmail.com>
2019-09-08 12:08:51 -0300

    Late Module Loading (Lazy-Loading)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
-import { AboutComponent } from "./about/about.component";
 import { RestaurantsComponent } from "./restaurants/restaurants.component";
 import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component";
 import { MenuComponent } from "./restaurant-detail/menu/menu.component";
@@ -11,16 +10,14 @@ import { OrderSummaryComponent } from "./order-summary/order-summary.component";
 export const ROUTES: Routes = [
   { path: "", component: HomeComponent },
   { path: "restaurants", component: RestaurantsComponent },
-  {
-    path: "restaurants/:id",
-    component: RestaurantDetailComponent,
-    children: [
+  { path: "restaurants/:id", component: RestaurantDetailComponent,
+  children: [
       { path: "", redirectTo: "menu", pathMatch: "full" },
       { path: "menu", component: MenuComponent },
       { path: "reviews", component: ReviewsComponent }
     ]
   },
-  { path: "about", component: AboutComponent },
+  { path: "about", loadChildren: "./about/about.module#AboutModule" },
   { path: "order-summary", component: OrderSummaryComponent},
   { path: "order", component: OrderComponent }
 ];
